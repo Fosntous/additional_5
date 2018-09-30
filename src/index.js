fedd6814c8ca0d20474ac62ebbad6127dc5e0514
Fosntous <43320126+Fosntous@users.noreply.github.com>
2018-09-30 22:45:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var ster = "";
+  for(i = 0; i < str.length;i++){
+    for(j = 0; j < bracketsConfig.length;j++){
+      if(str[i] == bracketsConfig[j][0])ster+=j;
+      if(str[i] == bracketsConfig[j][1]){
+        if(ster[ster.length-1] == j)
+          ster = ster.substring(0,ster.length-1);
+        /*else if(bracketsConfig[j][0] == bracketsConfig[j][1])
+          return false;*/
+      }
+    }
+  }
+  if(ster != "" || ster.length != 0)return false;
+  return true;
 }
